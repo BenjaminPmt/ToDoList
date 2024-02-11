5091a63b7e4502d50e19604a0a7e34d19d2dbfa0
BenjaminPmt <benjamin.pimont@laposte.net>
2024-02-11 14:57:05 +0100

    First commit

---
+++ pages/Home.js
@@ -0,0 +1,9 @@
+// This page is for see all the task and add the task when you click on the button +
+import { View, Text } from 'react-native'
+export default function Home() {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
